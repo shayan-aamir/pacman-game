429ed7f435fe699a9dba332fcb8a0b0a60c75e8c
shayan-aamir <145197091+shayan-aamir@users.noreply.github.com>
2024-07-08 22:27:04 +0500

    Add files via upload

---
+++ build/CMakeFiles/Pacman.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Pacman.dir/main.cpp.o"
+  "CMakeFiles/Pacman.dir/main.cpp.o.d"
+  "Pacman"
+  "Pacman.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Pacman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
